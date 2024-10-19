1066557368c4cfe2c96145deadddd4173b826d81
Valya <va100k02@mail.ru>
2024-10-19 23:16:23 +0700

    добавлена аутентификация при подключении к вебсокету по JWT

--- rip_desk/asgi.py
+++ rip_desk/asgi.py
@@ -3,12 +3,13 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from core import routing
+from core.channels_middleware import JwtAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rip_desk.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": JwtAuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
         )
