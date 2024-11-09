7264362e5c0bf99d6daf75964809592d5cd8f46b
Valya <va100k02@mail.ru>
2024-11-09 23:07:43 +0700

    решена проблема с подключением к БД

--- backend/manage.py
+++ backend/manage.py
@@ -2,7 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.db import connections
+from django.db.utils import OperationalError
+import time
 
 def main():
     """Run administrative tasks."""
@@ -15,6 +17,23 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    if "runserver" in sys.argv:
+        
+        while True:
+            try:
+                # Проверка подключения к базе данных
+                db_conn = connections['default']
+                db_conn.cursor()
+                break
+            except OperationalError:
+                print("Reconnecting to db")
+                time.sleep(1)
+        
+        execute_from_command_line(["manage.py", "makemigrations"])
+        execute_from_command_line(["manage.py", "migrate"])
+        
+
     execute_from_command_line(sys.argv)
 
 
